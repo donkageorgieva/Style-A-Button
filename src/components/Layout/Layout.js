aa451ebb63407d5e6c10a3c68d2c253d0691f7ad
donkageorgieva <DoniGeorgieva16@gmail.com>
2021-11-17 17:18:41 +0200

    feat: button preview window

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Header from "./Header/Header";
 import Controlls from "./Controlls/Controlls-Group";
+import PreviewWindow from "./Preview-Window/Preview-Window";
 const Layout = (props) => {
   return (
     <React.Fragment>
       <Header toggleMode={props.toggleMode} dark={props.dark} />
       <main className=" container mx-auto px-4 flex dark:bg-gray-900 bg-gray-50 w-full h-screen transition duration-400 items-center">
         <Controlls />
+        <PreviewWindow />
       </main>
     </React.Fragment>
   );
---
+++ src/components/Layout/Preview-Window/Preview-Window.js
@@ -0,0 +1,11 @@
+import Button from "../../UI/Button/Button";
+
+const PreviewWindow = (props) => {
+  return (
+    <div className="dark:bg-indigo-800 p-4">
+      <Button />
+    </div>
+  );
+};
+
+export default PreviewWindow;
