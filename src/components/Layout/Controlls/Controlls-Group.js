aab56cec8d976f8c7e67ae70838e07ab939b72e1
donkageorgieva <DoniGeorgieva16@gmail.com>
2021-11-19 14:36:16 +0200

    fix: buttons style

--- src/components/Layout/Controlls/Controlls-Group.js
+++ src/components/Layout/Controlls/Controlls-Group.js
@@ -1,12 +1,20 @@
+import React from "react";
+import Button from "../../UI/Button/Button";
 import Control from "./Control/Control";
 const Controlls = () => {
   return (
-    <div className="h-full flex flex-col justify-center  w-1/5 bg-gray-100 dark:bg-gray-900 p-4">
-      <div className=" bg-gray-200 dark:bg-gray-800 p-4">
-        <Control setting="padding" />
-        <Control />
-        <Control />
-        <Control />
+    <div className=" w-1/5 bg-gray-100 dark:bg-gray-900 h-full">
+      <div className="flex w-100">
+        <Button otherClasses="flex-grow" text="Button styles" />
+        <Button otherClasses="flex-grow" text="Hover styles" />
+      </div>
+      <div className="p-4 h-screen flex flex-col justify-center">
+        <div className=" bg-gray-200 dark:bg-gray-800 p-4">
+          <Control setting="padding" />
+          <Control />
+          <Control />
+          <Control />
+        </div>
       </div>
     </div>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,7 +1,13 @@
 const Button = (props) => {
   return (
     <button
-      className={props.bg ? props.bg : "bg-indigo-500"}
+      className={
+        props.classes
+          ? props.classes
+          : [
+              `px-4 py-2 dark:bg-gray-700 dark:hover:bg-indigo-500 dark:text-indigo-100 dark:focus:bg-indigo-500 ${props.otherClasses}`,
+            ].join("")
+      }
       onClick={props.onClick}
     >
       {props.text ? props.text : "Button"}
